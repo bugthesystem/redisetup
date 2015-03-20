f7917cae575cef14c63ad1106cf4dcc957b10760
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-03-20 10:52:56 +0200

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REDIS_VER=2.8.19
+UPDATE_LINUX_PACKAGES=false
+REDIS_INSTANCE_NAME=redis-server
+REDIS_INSTANCE_PORT=6379
+
+if [ ! -f redis-node-setup.sh ]
+then
+	wget https://github.com/ziyasal/redisetup/raw/master/member-setup.sh
+fi
+
+sudo sh member-setup.sh master $REDIS_VER $UPDATE_LINUX_PACKAGES $REDIS_INSTANCE_NAME $REDIS_INSTANCE_PORT
