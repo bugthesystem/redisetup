3d300abf6393b17e82f660d094e10fe6f9c7364b
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-03-24 09:56:48 +0200

    Create remove-sentinel.sh

---
+++ remove-sentinel.sh
@@ -0,0 +1,6 @@
+sudo rm  -rf /usr/local/bin/redis-sentinel*
+sudo rm -rf /etc/redis-sentinel/
+sudo rm -rf /var/log/redis-sentinel*
+sudo rm -rf /var/lib/redis-sentinel/
+sudo rm -rf /etc/init.d/redis-sentinel*
+sudo rm -rf /var/run/redis-sentinel*
